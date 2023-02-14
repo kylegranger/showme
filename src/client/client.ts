b7f1086797c8f53e7f1afc926a67b3a382fa331d
kylegranger <kyle.granger@chello.at>
2023-02-14 13:50:48 +0100

    feat: load state file

--- src/client/client.ts
+++ src/client/client.ts
@@ -2,13 +2,69 @@
 import { CApp } from './app'
 
 let app : CApp;
+let fileHandle: FileSystemFileHandle;
+async function main() {
+    [fileHandle] = await window.showOpenFilePicker();
 
-function main() {
-    app = new CApp(document.querySelector("#bancan"))
+    app = new CApp(document.querySelector("#bancan"), fileHandle)
     window['showmeapp'] = app;
 }
 
-main()
+document.getElementById("instructions").style.visibility = 'hidden';
+document.getElementById("overlayLeft").style.visibility = 'hidden';
+document.getElementById("overlayRight").style.visibility = 'hidden';
+document.getElementById("gradient").style.visibility = 'hidden';
+
+var clickme = document.getElementById("clickme");
+clickme.addEventListener("click", loadState, false);
+
+async function loadState() {
+    console.log('got click')
+
+
+    window.removeEventListener('mousedown', loadState, false);
+    let fileHandle: FileSystemFileHandle;
+    [fileHandle] = await window.showOpenFilePicker();
+
+    console.log('fileHandle: ', fileHandle);
+    var elem = document.getElementById('clickme');
+    elem.parentNode.removeChild(elem);
+    document.getElementById("instructions").style.visibility = 'visible';
+    document.getElementById("overlayLeft").style.visibility = 'visible';
+    document.getElementById("gradient").style.visibility = 'visible';
+
+
+    app = new CApp(document.querySelector("#bancan"), fileHandle)
+    window['showmeapp'] = app;
+
+    // fileHandle.getFile().then( async (file) => {
+    //     const contents = await file.text();
+    //     handleStateText(contents);
+    // });
+    // clickme
+
+    // app = new CApp(document.querySelector("#bancan"), fileHandle)
+    // window['showmeapp'] = app;
+    
+    // evt.preventDefault();
+}
+
+
+// // main();
+// async function loadState(evt: MouseEvent) {
+//     console.log('got click')
+
+//     window.removeEventListener('mousedown', loadState, false);
+//     let fileHandle: FileSystemFileHandle;
+//     [fileHandle] = await window.showOpenFilePicker();
+
+//     app = new CApp(document.querySelector("#bancan"), fileHandle)
+//     window['showmeapp'] = app;
+    
+//     evt.preventDefault();
+// }
+
+// window.addEventListener('mousedown', loadState, false);
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -31,7 +87,7 @@ window.addEventListener('contextmenu', function (e) {
 }, false);
 
 var animate = function () {
-    app.render();
+    if (app) app.render();
     requestAnimationFrame(animate);
 };
 
